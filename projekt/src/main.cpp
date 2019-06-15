182e50ea5123ef8d30d535fbaf22b593f27c9163
Jules7 <akj123429@gmail.com>
2019-06-15 09:21:45 +0200

    Projekt dir added

---
+++ projekt/src/main.cpp
@@ -0,0 +1,67 @@
+//
+// Created by hp on 6/9/19.
+//
+
+#include <iostream>
+#include "grap.h"
+
+
+int main()
+{
+
+
+
+
+
+
+    return EXIT_SUCCESS;
+}
+
+
+
+
+//int main()
+//{
+//
+////    GraphenTheorie::Graph graph;
+////    graph.addNode('a', 'b', 10);
+////    std::cout << graph << '\n';
+//
+//    GraphenTheorie::Graph graph(4, 3);
+//    std::cout << graph << std::endl;
+//
+//    auto graph1 = GraphenTheorie::maximalerSpannbaum(graph);
+//    std::cout << graph1 << std::endl;
+//
+//    return EXIT_SUCCESS;
+//}
+
+
+
+
+
+
+//#include <SFML/Graphics.hpp>
+//
+//int main()
+//{
+//    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+//    sf::CircleShape shape(100.f);
+//    shape.setFillColor(sf::Color::Green);
+//
+//    while (window.isOpen())
+//    {
+//        sf::Event event;
+//        while (window.pollEvent(event))
+//        {
+//            if (event.type == sf::Event::Closed)
+//                window.close();
+//        }
+//
+//        window.clear();
+//        window.draw(shape);
+//        window.display();
+//    }
+//
+//    return 0;
+//}
